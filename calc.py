6fb775179fc88166d0be74279052015a1082b332
kpark0435 <166029246+kpark0435@users.noreply.github.com>
2024-04-04 23:08:56 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,24 @@
+def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    z = divide(x, y)
 
+    if y != 0:
+        print("%d / %d = %0.3f" % (x, y, z))
+    
+    
+def add(a, b):
+    return a + b
+    
+    
+# TODO: divide() 함수 정의
+def divide(a, b):
+    if b == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        return a/b
+
+if __name__ == "__main__":
+    main()
